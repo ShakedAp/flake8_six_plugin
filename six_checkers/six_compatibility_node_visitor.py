b87cb4e18fa06dcd745ad1e7c0bf2b3ccf37f41c
ShakedAp <shaked.apter123@gmail.com>
2025-03-29 00:50:24 +0300

    Update SixCompatibilityNodeVisitor to auto-populate with visit functions

--- six_checkers/six_compatibility_node_visitor.py
+++ six_checkers/six_compatibility_node_visitor.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
 import ast
+from typing import Dict, Tuple, Iterable
 
 from flake8_errors_info import SIXErrorInfo
+from six_checkers.six_checker import SixChecker
 from six_checkers.unallowed_name_checkers import (
     InternNotAllowedChecker,
     ReloadNotAllowedChecker,
@@ -9,22 +12,74 @@ from six_checkers.unallowed_name_checkers import (
 )
 
 
-class SixCompatibilityNodeVisitor(ast.NodeVisitor):
+NODE_VISITOR_VISIT_METHOD_FORMAT = "visit_{}"
+
+
+def _create_visit_method(checkers: Iterable[SixChecker]) -> callable:
+    """Create the visit method from the given checkers.
+
+    Args:
+        checkers (Iterable[SixChecker]): The checkers to run on each relevant node visit.
+
+    Returns:
+        callable: A visit method that runs the check function on all the given checkers and calls generic_visit at the end.
+    """
+
+    def visit(self: ast.NodeVisitor, node: ast.stmt) -> None:
+        for checker in checkers:
+            checker.check(node, self.errors)
+
+        self.generic_visit(node)
+
+    return visit
+
+
+def _add_node_checkers_to_methods(
+    node_checkers: Dict[str, Tuple[SixChecker]], dct: dict
+) -> None:
+    """
+    Add ast Node Visitor visit functions from the given node_checkers.
+
+    Args:
+        node_checkers (Dict[str, Tuple[SixChecker]]): A dictionary that maps between the visit function name and the checkers to run.
+        dct (dict): The attribute dicts to be updated.
+    """
+    for node_name, checkers in node_checkers.items():
+        method_name = NODE_VISITOR_VISIT_METHOD_FORMAT.format(node_name)
+        dct[method_name] = _create_visit_method(checkers)
+
+
+class NodeCheckerAdderMeta(type):
+    """
+    A metaclass populates all of the ast Node Visitor visit functions from the defined node_checkers.
+    """
+
+    def __new__(cls, name, bases, dct):
+        node_checkers = dct.get("node_checkers", {})
+        _add_node_checkers_to_methods(node_checkers, dct)
+
+        return super().__new__(cls, name, bases, dct)
+
+
+class SixCompatibilityNodeVisitor(ast.NodeVisitor, metaclass=NodeCheckerAdderMeta):
     """
     An ast node visitor that keeps track of any errors found by checkers.
+
+    The node visitors are populated using the metaclass, and the node_checkers dict.
+    Each key in the dict is being used to created the corresponding visit function, and calls the check method for all
+    of the listed checkers.
+
+    For example, adding Call will create visit_Call that will run all it's checkers.
     """
+
+    node_checkers: Dict[str, Tuple[SixChecker]] = {
+        "Call": (
+            InternNotAllowedChecker,
+            ReloadNotAllowedChecker,
+            CoerceNotAllowedChecker,
+        ),
+        "FunctionDef": (CoerceMethodNotAllowedChecker,),
+    }
 
     def __init__(self):
         self.errors: list[SIXErrorInfo] = []
-
-    def visit_Call(self, node: ast.Call) -> None:
-        InternNotAllowedChecker.check(node, self.errors)
-        ReloadNotAllowedChecker.check(node, self.errors)
-        CoerceNotAllowedChecker.check(node, self.errors)
-
-        self.generic_visit(node)
-
-    def visit_FunctionDef(self, node):
-        CoerceMethodNotAllowedChecker.check(node, self.errors)
-
-        self.generic_visit(node)
